f3716d7db9ac681ba94133259012e558824d7eeb
modusinternet <vince@modusinternet.com>
2024-11-15 19:33:31 -0800

    asdf

--- ccmstpl/_js/main.js
+++ ccmstpl/_js/main.js
@@ -1,7 +1,4 @@
-<?php
-header("Content-Type: application/javascript; charset=utf-8");
-header("Service-Worker-Allow: /");
-?>{CCMS_DB_PRELOAD:all,contact-us}/* Compress using https://jscompress.com/ */
+{CCMS_DB_PRELOAD:all,contact-us}/* Compress using https://jscompress.com/ */
 
 /* nav bar active selector */
 navActiveArray.forEach(function(s){$("#"+s).addClass("active");});
